4532036f83862612599d23f6e970e9f2acb4bd9f
Christina0104 <cm.Hasenhuettl@gmx.at>
2025-06-02 09:15:00 +0000

    Verbesserung Test

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -1,11 +1,64 @@
 #include <stdio.h>
 
+int int_even_odd(int a)
+{
+  if (a == 0)
+    return 0;
+  else if (a % 2 == 0)
+    return 2;
+  else
+    return 1;
+}
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   printf("This program was called with %d arguments.\n", argc);
   printf("Hello World!\n");
 
+  //EXERCISE 1
+  int number = 0, result = 0;
+  result = int_even_odd(number);
+  printf("Testzahl : ");
+  scanf("%d", &number);
+  result = int_even_odd(number);
+  if (result == 0)
+    printf("Die Testzahl ist 0\n");
+  else if (result == 1)
+    printf("Die Testzahl ist ungerade.\n");
+  else
+    printf("Die Testzahl ist gerade.\n");
+
+//EXERCISE 2
+int numbers[10] = {3, 1, 7, 4, 9, 1, 5, 10, -2, 2} ;
+for( int i = 0; i < 10; i++)
+{
+  printf("Zahl %d: %d\n" , i+1, numbers[i]);
+}
+
+//EXERCISE 3
+int num_arr[5];
+int count_even = 0, count_odd = 0, sum= 0;
+
+for (int i = 0; i<5; i++)
+{
+  printf("%d. Zahl: ", i+1);
+ scanf("%d", &num_arr[i]);
+}
+for (int i = 0; i < 5; i++)
+{
+  sum += num_arr[i];
+  if (num_arr[i] % 2 == 0 ) count_even++;      // --> dann gerade
+}
+
+count_odd = 5 - count_even;
+
+printf("Anzahl der geraden Zahlen: %d\n" , count_even);
+printf("Anzahl der ungeraden Zahlen: %d\n" , count_odd);
+printf("Summe der Zahlen: %d\n" , sum);
+
+
+
+  /*
 //-----Exercise 2-----
 
 int array[10] = {3, 1, 7, 4, 9, 1, 5, 10, -2, 2};
@@ -36,7 +89,7 @@ else if (int zahl = 0)
 {
   return 0;
 }
-  
+
 
 //-----exercise 3------
 
@@ -69,6 +122,5 @@ for ( int i = 0 ; i < 5; i++);
 {
   printf("%d , int_numbers[i]");
 }
-
-
+*/
 }
